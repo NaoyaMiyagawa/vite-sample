681a994e9b61b894e5616ca0ddf8610fe515950c
NaoyaMiyagawa <tsuna2764@gmail.com>
2021-08-20 10:09:54 +0900

    feat(router): add vue-router

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+
+  <div>hoge</div>
+</template>
