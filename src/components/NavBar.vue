65fb5d91f879d414988a5e5ece3fb5c4bacd3884
NaoyaMiyagawa <tsuna2764@gmail.com>
2021-08-21 11:24:11 +0900

    feat(nav): emphasize current page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,37 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+    type Route = { to: string; name: string };
+    const routes = ref<Route[]>([
+      { to: '/', name: 'Home' },
+      { to: '/population', name: 'Population' },
+      { to: '/cat', name: 'Cat' },
+    ]);
+    const isCurrent = (path: string) => {
+      return route.path === path;
+    };
+
+    return { route, routes, isCurrent };
+  },
+});
+</script>
+
 <template>
   <nav class="p-3 mb-5 bg-blue-400 text-teal-50 flex justify-center">
-    <div class="max-w-screen-sm space-x-20">
-      <router-link to="/">Home</router-link>
-      <router-link to="/population">Population</router-link>
-      <router-link to="/cat">Cat</router-link>
+    <div class="max-w-screen-sm">
+      <template v-for="route in routes">
+        <router-link
+          :to="route.to"
+          class="px-10 py-3 hover:(text-blue-700 bg-light-blue-400)"
+          :class="{ 'text-light-blue-300 font-bold': isCurrent(route.to) }"
+        >
+          {{ route.name }}
+        </router-link>
+      </template>
     </div>
   </nav>
 </template>
