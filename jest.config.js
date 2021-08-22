d81e3fa1c2aa0163f24741212411109c67af8175
NaoyaMiyagawa <tsuna2764@gmail.com>
2021-08-22 20:47:20 +0900

    test: fix @ path resolve with jest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  // roots: ['<rootDir>/src'],
-  // testMatch: [
-  //   //
-  //   '**/__tests__/**/*.+(ts|tsx|js)',
-  //   '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  // ],
+  roots: ['<rootDir>'],
+  testMatch: [
+    //
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.vue$': 'vue-jest',
   },
   moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   // preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.+)$': '<rootDir>/src/$1',
   },
